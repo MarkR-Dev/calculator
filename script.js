942d0bdea9ed9418b69369cec0a4f1a1d66659db
Mark Rodgers <markrodgers.dev@gmail.com>
2023-10-26 16:23:24 +0100

    Add the functions that will handle the calculations

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 "use strict"
 
+const calculator = {
+    add(a, b){
+        return a + b;
+    },
+    subtract(a, b){
+        return a - b;
+    },
+    multiply(a, b){
+        return a * b;
+    },
+    divide(a, b){
+        return a / b;
+    },
+    operate(operator, num1, num2){
+        return calculator[operator](num1, num2);
+    }
+}
+
