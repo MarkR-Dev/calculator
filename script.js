2f28d0f4038d6ce532b3080b598200a8f7cbc97f
Mark Rodgers <markrodgers.dev@gmail.com>
2023-10-27 17:01:07 +0100

    Add JS to allow basic calculations to be made

--- script.js
+++ script.js
@@ -19,3 +19,60 @@ const calculator = {
     }
 }
 
+const display = document.querySelector("#display");
+const inputs = document.querySelectorAll(".input");
+const operators = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector("#equals");
+const clearButton = document.querySelector("#clear");
+let operandOne = "";
+let operandTwo = "";
+let operatorSymbol = "";
+
+
+inputs.forEach(input => {
+  input.addEventListener("click", processInput);
+});
+    
+operators.forEach(operator => {
+  operator.addEventListener("click", (event) => {
+    operatorSymbol = event.target.id;
+    console.log(operandOne, operatorSymbol, operandTwo)
+  })
+})
+
+equalsButton.addEventListener("click", calculate);
+ 
+function updateDisplay(num){
+  display.textContent = num;
+}
+
+clearButton.addEventListener("click", clear);
+
+function processInput(event){
+    
+    if(operatorSymbol === ""){
+      operandOne += event.target.textContent;
+      updateDisplay(operandOne);
+    }else{
+      operandTwo += event.target.textContent;
+      updateDisplay(operandTwo);
+    }
+    console.log(operandOne, operatorSymbol, operandTwo)
+}
+
+function calculate(){
+  operandOne = Number(operandOne);
+  operandTwo = Number(operandTwo);
+  const result = calculator.operate(operatorSymbol, operandOne, operandTwo);
+  updateDisplay(result);
+  console.log(operandOne, operatorSymbol, operandTwo, result)
+}
+
+function clear(){
+  operandOne = "";
+  operandTwo = "";
+  operatorSymbol = "";
+  updateDisplay("");
+}
+
+
